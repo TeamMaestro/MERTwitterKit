ef6cca661556a64d00296f21f6675ecacb755d31
William Towe <willbur1984@gmail.com>
2014-04-16 17:33:50 -0400

    clarification in Favorites category

--- MERTwitterKit/MERTwitterClient+Favorites.h
+++ MERTwitterKit/MERTwitterClient+Favorites.h
@@ -30,7 +30,7 @@
  @param afterIdentity The identity of the tweet for which to return favorites whose identity is greater than (i.e. newer) _afterIdentity_
  @param beforeIdentity The identity of the tweet for which to return favorites whose identity is less than (i.e. older) _beforeIdentity_
  @param count The maximum number of favorites the request should return. The default is 20
- @exception NSException Thrown if _identity_ and _screenName_ are nil
+ @exception NSException Thrown if _identity_ is <= 0 and _screenName_ is nil
  */
 - (RACSignal *)requestFavoritesForUserWithIdentity:(int64_t)identity screenName:(NSString *)screenName afterIdentity:(int64_t)afterIdentity beforeIdentity:(int64_t)beforeIdentity count:(NSUInteger)count;
 
@@ -40,7 +40,7 @@
  More information can be found at https://dev.twitter.com/docs/api/1.1/post/favorites/create
  
  @param identity The identity of the tweet to favorite
- @exception NSException Thrown if _identity_ is nil
+ @exception NSException Thrown if _identity_ is <= 0
  */
 - (RACSignal *)requestFavoriteCreateForTweetWithIdentity:(int64_t)identity;
 /**
@@ -49,7 +49,7 @@
  More information can be found at https://dev.twitter.com/docs/api/1.1/post/favorites/destroy
  
  @param identity The identity of the tweet to un-favorite
- @exception NSException Thrown if _identity_ is nil
+ @exception NSException Thrown if _identity_ is <= 0
  */
 - (RACSignal *)requestFavoriteDestroyForTweetWithIdentity:(int64_t)identity;
 
