dca7b77f1b2537630b447670c465254386206d1e
William Towe <willbur1984@gmail.com>
2014-04-16 02:01:05 -0400

    comment the FriendsAndFollowers category

--- MERTwitterKit/MERTwitterClient+FriendsAndFollowers.h
+++ MERTwitterKit/MERTwitterClient+FriendsAndFollowers.h
@@ -13,6 +13,15 @@
 
 #import "MERTwitterClient.h"
 
+/**
+ Mask describing the friendship status of a user related to the authenticated user.
+ 
+ - `MERTwitterClientFriendshipStatusNone`, no friendship :(
+ - `MERTwitterClientFriendshipStatusFollowing`, the authenticated user is following the user
+ - `MERTwitterClientFriendshipStatusFollowingRequested`, the authenticated user has a following request pending for the user
+ - `MERTwitterClientFriendshipStatusFollowedBy`, the authenticated user is followed by the user
+ - `MERTwitterClientFriendshipStatusBlocking`, the authenticated user is blocking the user
+ */
 typedef NS_OPTIONS(NSInteger, MERTwitterClientFriendshipStatus) {
     MERTwitterClientFriendshipStatusNone = 0,
     MERTwitterClientFriendshipStatusFollowing = 1 << 0,
@@ -21,15 +30,91 @@ typedef NS_OPTIONS(NSInteger, MERTwitterClientFriendshipStatus) {
     MERTwitterClientFriendshipStatusBlocking = 1 << 3
 };
 
+/**
+ Methods to interact with the _friends_, _followers_, and _friendships_ resource families of the Twitter API.
+ */
 @interface MERTwitterClient (FriendsAndFollowers)
 
+/**
+ Returns a signal that sends `next` with a `MERTwitterUserViewModel` object representing the friended user, then `completes`. If the request cannot be completed, sends `error`.
+ 
+ You must provide either _identity_ or _screenName_.
+ 
+ More information can be found at https://dev.twitter.com/docs/api/1.1/post/friendships/create
+ 
+ @param identity The identity of the user to request friendship for
+ @param screenName The screen name of the user to request friendship for
+ @exception NSException Thrown if _identity_ and _screenName_ are nil
+ @see requestFriendshipDestroyForUserWithIdentity:screenName:
+ */
 - (RACSignal *)requestFriendshipCreateForUserWithIdentity:(int64_t)identity screenName:(NSString *)screenName;
+/**
+ Returns a signal that sends `next` with a `MERTwitterUserViewModel` object representing the un-friended user, then `completes`. If the request cannot be completed, sends `error`.
+ 
+ You must provide either _identity_ or _screenName_.
+ 
+ More information can be found at https://dev.twitter.com/docs/api/1.1/post/friendships/destroy
+ 
+ @param identity The identity of the user to destroy friendship for
+ @param screenName The screen name of the user to destroy friendship for
+ @exception NSException Thrown if _identity_ and _screenName_ are nil
+ @see requestFriendshipCreateForUserWithIdentity:screenName:
+ */
 - (RACSignal *)requestFriendshipDestroyForUserWithIdentity:(int64_t)identity screenName:(NSString *)screenName;
 
+/**
+ Returns a signal that sends `next` with a `RACTuple` containing an array of `MERTwitterUserViewModel` instances, the next _cursor_ value, and the previous _cursor_ value, then `completes`. If the request cannot be completed, sends `error`.
+ 
+ You must provide either _identity_ or _screenName_.
+ 
+ More information can be found at https://dev.twitter.com/docs/api/1.1/get/friends/list
+ 
+ @param identity The identity of user for which to request friends
+ @param screenName The screen name of the user for which to request friends
+ @param count The maximum number of friends the request should return. The default is 20
+ @param cursor The cursor value that determines which page of friends to return. The default value is MERTwitterClientCursorInitial, which means "return the first page"
+ @exception NSException Thrown if _identity_ and _screenName_ are nil
+ @see requestFollowersForUserWithIdentity:screenName:count:cursor:
+ */
 - (RACSignal *)requestFriendsForUserWithIdentity:(int64_t)identity screenName:(NSString *)screenName count:(NSUInteger)count cursor:(int64_t)cursor;
+/**
+ Returns a signal that sends `next` with a `RACTuple` containing an array of `MERTwitterUserViewModel` instances, the next _cursor_ value, and the previous _cursor_ value, then `completes`. If the request cannot be completed, sends `error`.
+ 
+ You must provide either _identity_ or _screenName_.
+ 
+ More information can be found at https://dev.twitter.com/docs/api/1.1/get/followers/list
+ 
+ @param identity The identity of the user for which to request followers
+ @param screenName The screen name of the user for which to request followers
+ @param count The maximum number of followers the request should return. The default is 20
+ @param cursor The cursor value that determines which page of followers to return. The default value is MERTwitterClientCursorInitial, which means "return the first page"
+ @exception NSException Thrown if _identity_ and _screenName_ are nil
+ @see requestFriendsForUserWithIdentity:screenName:count:cursor:
+ */
 - (RACSignal *)requestFollowersForUserWithIdentity:(int64_t)identity screenName:(NSString *)screenName count:(NSUInteger)count cursor:(int64_t)cursor;
 
+/**
+ Calls `requestFriendshipStatusForUsersWithIdentities:screenNames:`, passing `@[@(identity)]` and/or `@[screenName]`.
+ 
+ You must provide either _identity_ or _screenName_.
+ 
+ @param identity The identity of the user for which to request friendship status
+ @param screenName The screen name of the user for which to request friendship status
+ @exception NSException Thrown if _identity_ and _screenName_ are nil
+ @see requestFriendshipStatusForUsersWithIdentities:screenNames:
+ */
 - (RACSignal *)requestFriendshipStatusForUserWithIdentity:(int64_t)identity screenName:(NSString *)screenName;
+/**
+ Returns a signal that sends `next` with an array of `RACTuple` instances, each containing a `MERTwitterUserViewModel` instance and a `MERTwitterClientFriendshipStatus` value, indicating the friendship status of the user related to the authenticated user. If the request cannot be completed, sends `error`.
+ 
+ You must provide either _identities_ or _screenNames_.
+ 
+ More information can be found at https://dev.twitter.com/docs/api/1.1/get/friendships/lookup
+ 
+ @param identities The array of identities for which to request friendship status. Must satisfy `0 < [identities count] <= 100`.
+ @param screenNames The array of screen names for which to request friendship status. Must satisfy `0 < [screenNames count] <= 100`.
+ @exception NSException Thrown if _identities_ and _screenNames_ are nil
+ */
 - (RACSignal *)requestFriendshipStatusForUsersWithIdentities:(NSArray *)identities screenNames:(NSArray *)screenNames;
 
 @end
