5a2df839b63e10eea2a07f5dc21f7eca03d4e55d
William Towe <willbur1984@gmail.com>
2014-04-18 11:16:52 -0400

    finish commenting MERTwitterTweetViewModel

--- MERTwitterKit/MERTwitterTweetViewModel.h
+++ MERTwitterKit/MERTwitterTweetViewModel.h
@@ -16,26 +16,109 @@
 
 @class MERTwitterUserViewModel;
 
+/**
+ `MERTwitterTweetViewModel` is a `RVMViewModel` subclass that represents a Tweet returned from the Twitter API.
+ 
+ More information can be found at https://dev.twitter.com/docs/platform-objects/tweets
+ */
 @interface MERTwitterTweetViewModel : RVMViewModel
 
+/**
+ The identity of the Tweet. This is unique.
+ 
+ The `id` of the Tweet JSON object.
+ */
 @property (readonly,nonatomic) int64_t identity;
 
+/**
+ The UTF-8 text of the Tweet.
+ 
+ The `text` of the Tweet JSON object.
+ */
 @property (readonly,nonatomic) NSString *text;
+/**
+ The set of `NSValue` instances wrapping `NSRange` structures representing the locations of hashtags within the receiver's `text` property.
+ 
+ The `entities.hashtags` of the Tweet JSON object.
+ 
+ More information can be found at https://dev.twitter.com/docs/entities#tweets
+ */
 @property (readonly,nonatomic) NSSet *hashtagRanges;
+/**
+ The set of `NSValue` instances wrapping `NSRange` structures representing the locations of media within the receiver's `text` property.
+ 
+ The `entities.media` of the Tweet JSON object.
+ 
+ More information can be found at https://dev.twitter.com/docs/entities#tweets
+ */
 @property (readonly,nonatomic) NSSet *mediaRanges;
+/**
+ The set of `NSValue` instances wrapping `NSRange` structures representing the locations of mentions (e.g. @screenname) within the receiver's `text` property.
+ 
+ The `entities.user_mentions` of the Tweet JSON object.
+ 
+ More information can be found at https://dev.twitter.com/docs/entities#tweets
+ */
 @property (readonly,nonatomic) NSSet *mentionRanges;
+/**
+ The set of `NSValue` instances wrapping `NSRange` structures representing the locations of financial symbols within the receiver's `text` property.
+ 
+ The `entities.symbols` of the Tweet JSON object.
+ 
+ More information can be found at https://dev.twitter.com/docs/entities#tweets
+ */
 @property (readonly,nonatomic) NSSet *symbolRanges;
+/**
+ The set of `NSValue` instances wrapping `NSRange` structures representing the locations of urls within the receiver's `text` property.
+ 
+ The `entities.urls` of the Tweet JSON object.
+ 
+ More information can be found at https://dev.twitter.com/docs/entities#tweets
+ */
 @property (readonly,nonatomic) NSSet *urlRanges;
 
+/**
+ The date the receiver was created at.
+ 
+ The `created_at` of the Tweet JSON object.
+ */
 @property (readonly,nonatomic) NSDate *createdAt;
+/**
+ A string representing the `createdAt` property.
+ */
 @property (readonly,nonatomic) NSString *relativeCreatedAtString;
 
+/**
+ The approximate favorite count of the Tweet.
+ 
+ The `favorite_count` of the Tweet JSON object.
+ */
 @property (readonly,nonatomic) NSUInteger favoriteCount;
+/**
+ The approximate retweet count of the Tweet.
+ 
+ The `retweet_count` of the Tweet JSON object.
+ */
 @property (readonly,nonatomic) NSUInteger retweetCount;
 
+/**
+ A thumbnail of the receiver's attached media, if any.
+ 
+ The property is initially nil, and is fetched asynchronously once the receiver's `active` property is set to YES. You should bind to this property with ReactiveCocoa to be notified when the thumbanil is fetched.
+ 
+ See `MERTweetTableViewCell` within the MERTwitterKitDemo group for an example.
+ */
 @property (readonly,strong,nonatomic) UIImage *mediaThumbnailImage;
 
+/**
+ The `MERTwitterUserViewModel` instance that represents the owning user of the Tweet.
+ */
 @property (readonly,strong,nonatomic) MERTwitterUserViewModel *userViewModel;
+/**
+ The array of discussion tweets rooted at the receiver.
+ 
+ @see [MERTwitterClient fetchThreadForTweetWithIdentity]
+ */
 @property (readonly,nonatomic) NSArray *threadTweets;
 
 @end
